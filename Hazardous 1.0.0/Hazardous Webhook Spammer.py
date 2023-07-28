eac559db78da3fb49ed3c2bcdebe06780b892e11
Azuuu1 <136928945+Azuuu1@users.noreply.github.com>
2023-07-28 18:03:41 +0200

    Update Hazardous Webhook Spammer.py

--- Hazardous 1.0.0/Hazardous Webhook Spammer.py
+++ Hazardous 1.0.0/Hazardous Webhook Spammer.py
@@ -1,8 +1,8 @@
 from pystyle import *
+from colorama import *
 import requests
 import time
-from colorama import Fore, Style
-
+#new versionn :00
 def spammer():
     try:
         webhook = input("Webhook:")
@@ -20,43 +20,27 @@ def spammer():
     except requests.exceptions.MissingSchema:
         print(f"{Fore.LIGHTRED_EX}[!] Wrong Webhook{Style.BRIGHT}")
         time.sleep(2)
-import requests
 
 def check_webhook():
     webhook_url = input("Enter the webhook URL: ")
     response = requests.get(webhook_url)
-    
+
     if response.status_code == 200:
-        print(f"{Fore.GREEN}[+]Webhook works{Style.RESET_ALL}")
+        print(f"{Fore.GREEN}[+] Webhook works{Style.RESET_ALL}")
     else:
-        print(f"{Fore.LIGHTRED_EX}[-]Webhook does not work{Style.BRIGHT}")
+        print(f"{Fore.LIGHTRED_EX}[-] Webhook does not work{Style.BRIGHT}")
         time.sleep(2)
-    
 
+def delwebhook():
+    webhook_url = input("Enter the webhook URL to delete: ")
+    response = requests.delete(webhook_url)
+    if response.status_code == 204:
+        print("Webhook deleted successfully.")
+    else:
+        print("Failed to delete webhook. Status code:", response.status_code)
 
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-options = ("|1 Webhook Spammer|2 Webhook checker|3 Github")
-
-
-
-
-
-
-
-text = """
+options = ("|1 Webhook Spammer|2 Webhook checker|3 Webhook Deleter|4 Github")
+intro = """
  _   _                        _                   _    _      _     _                 _      _____                                           
 | | | |                      | |                 | |  | |    | |   | |               | |    /  ___|                                          
 | |_| | __ _ ______ _ _ __ __| | ___  _   _ ___  | |  | | ___| |__ | |__   ___   ___ | | __ \ `--. _ __   __ _ _ __ ___  _ __ ___   ___ _ __ 
@@ -64,28 +48,37 @@ text = """
 | | | | (_| |/ / (_| | | | (_| | (_) | |_| \__ \ \  /\  /  __/ |_) | | | | (_) | (_) |   <  /\__/ / |_) | (_| | | | | | | | | | | |  __/ |   
 \_| |_/\__,_/___\__,_|_|  \__,_|\___/ \__,_|___/  \/  \/ \___|_.__/|_| |_|\___/ \___/|_|\_\ \____/| .__/ \__,_|_| |_| |_|_| |_| |_|\___|_|   
                                                                                                   | |                                        
-                                                                                                  |_|                                       
+                                                                                                  |_|      
+                                                Press Enter                                             
+
 """
 
+Anime.Fade(Center.Center(intro), Colors.black_to_red, Colorate.Vertical, interval=0.035, enter=True)
 
 
+print(f"""{Fore.LIGHTRED_EX}
+ _   _                        _                   _    _      _     _                 _      _____                                           
+| | | |                      | |                 | |  | |    | |   | |               | |    /  ___|                                          
+| |_| | __ _ ______ _ _ __ __| | ___  _   _ ___  | |  | | ___| |__ | |__   ___   ___ | | __ \ `--. _ __   __ _ _ __ ___  _ __ ___   ___ _ __ 
+|  _  |/ _` |_  / _` | '__/ _` |/ _ \| | | / __| | |/\| |/ _ \ '_ \| '_ \ / _ \ / _ \| |/ /  `--. \ '_ \ / _` | '_ ` _ \| '_ ` _ \ / _ \ '__|
+| | | | (_| |/ / (_| | | | (_| | (_) | |_| \__ \ \  /\  /  __/ |_) | | | | (_) | (_) |   <  /\__/ / |_) | (_| | | | | | | | | | | |  __/ |   
+\_| |_/\__,_/___\__,_|_|  \__,_|\___/ \__,_|___/  \/  \/ \___|_.__/|_| |_|\___/ \___/|_|\_\ \____/| .__/ \__,_|_| |_| |_|_| |_| |_|\___|_|   
+                                                                                                  | |                                        
+                                                                                                  |_|        
+                                                Press Enter     
 
-
-
-print(Center.XCenter("Welcome To Hazardous Webhook Spammer Made By Azuu#6735"))
-print(Center.XCenter(text))
+""")
 print(Center.XCenter(options))
-select = input("Which option u wanna use:")
-if select == ("1"):
+select = input("Which option do you want to use: ")
+
+if select == "1":
     spammer()
-if select == ("2"):
+elif select == "2":
     check_webhook()
-if select == ("3"):
-    print("123")
-
-
-
-
-
-
-
+elif select == "3":
+    delwebhook()
+elif select == "4":
+    print("https://github.com/Azuuu1/Hazardous-Webhook-Spammer/blob/main/Hazardous%201.0.0/Hazardous%20Webhook%20Spammer.py")
+else:
+    print("Didn't find the option")
+time.sleep(2)
